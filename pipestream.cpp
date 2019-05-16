ba106177c2d1b2570fcaad8975bf906b881a2d66
Sapfir0 <sapfir999999@yandex.ru>
2019-05-17 00:43:29 +0400

    ы

--- pipestream.cpp
+++ pipestream.cpp
@@ -6,8 +6,6 @@
 
 bool PipeStream::open(QString filename, uint32_t flags) {
     DWORD openMode = 0, pipeMode = 0;
-    DWORD bufferSizeIn = 1024, bufferSizeOut = 1024, maxInstances = 254, timeOut = 5000;
-
 
     pipeMode = PIPE_TYPE_MESSAGE | PIPE_WAIT;
     pipeMode |= PIPE_READMODE_MESSAGE;
@@ -23,21 +21,17 @@ bool PipeStream::open(QString filename, uint32_t flags) {
         file = CreateNamedPipeA(filename.toStdString().c_str(),
                          openMode,
                          pipeMode,
-                         maxInstances,
-                         bufferSizeIn,
-                         bufferSizeOut,
-                         timeOut,
+                         254,
+                         1024,
+                         1024,
+                         5000,
                          nullptr);
     } else {
-        // игра классов
         FileStream game;
         game.open(filename, flags);
-        //PipeStream &next_game = reinterpret_cast <PipeStream&>(game);
-        //this->file = next_game.file;
         file = game.getHandle();
         game.setHandle(NULL);
     }
-
 
     bool fileOpen = file != INVALID_HANDLE_VALUE;
 
@@ -46,8 +40,4 @@ bool PipeStream::open(QString filename, uint32_t flags) {
     }
 
     return fileOpen;
-}
-
-bool PipeStream::waitingClient() {
-    return ConnectNamedPipe(file, nullptr);
 }
